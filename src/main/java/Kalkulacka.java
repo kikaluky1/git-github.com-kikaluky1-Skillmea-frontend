40be346d3415bee01bcbacf63c9abbdc3f3843c4
kikaluky1 <kikaluky1@gmail.com>
2025-11-06 20:01:24 +0100

    pridani testu a code behind

--- src/main/java/Kalkulacka.java
+++ src/main/java/Kalkulacka.java
@@ -5,12 +5,15 @@ import io.cucumber.java.en.When;
 
 public class Kalkulacka {
 
+    private int prvCislo;
+    private int druCislo;
+    private int vypoctenyVysledek;
 
     @Given("Uzivatel ma zadany dve cisla {int} a {int}")
     public void uzivatelMaZadanyDveCislaA(int prvniCislo, int druheCislo) {
-
+        prvCislo = prvniCislo;
+        druCislo = druheCislo;
     }
-
 
     @When("Uzivatel secte tato dve cisla")
     public void uzivatelSecteTatoDveCisla() {
