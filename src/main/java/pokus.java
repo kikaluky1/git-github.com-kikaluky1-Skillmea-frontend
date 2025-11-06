49cba32aefc7ec9636cd0e6a80a8bf1dc9ed2b0e
kikaluky1 <kikaluky1@gmail.com>
2025-11-06 19:43:32 +0100

    prvy pokus commit

---
+++ src/main/java/pokus.java
@@ -0,0 +1,42 @@
+import io.cucumber.java.PendingException;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class pokus {
+    @When("Toto je akcni krok")
+    public void totoJeAkcniKrok() {
+
+    }
+
+    @Given("Toto je nastaveni pocatecnich podminek")
+    public void totoJeNastaveniPocatecnichPodminek() {
+
+    }
+
+    @And("Toto je dalsi nastaveni pocatecnich podminek")
+    public void totoJeDalsiNastaveniPocatecnichPodminek() {
+
+    }
+
+    @And("Toto je dalsi akcni krok")
+    public void totoJeDalsiAkcniKrok() {
+
+    }
+
+    @And("Toto je jeste dalsi akcni krok")
+    public void totoJeJesteDalsiAkcniKrok() {
+
+    }
+
+    @Then("Toto je overovaci krok")
+    public void totoJeOverovaciKrok() {
+
+    }
+
+    @And("Toto je dalsi overovaci krok")
+    public void totoJeDalsiOverovaciKrok() {
+
+    }
+}
--- src/main/resources/pokus.feature
+++ src/main/resources/pokus.feature
@@ -1,7 +1,16 @@
 Feature: pokus
 
+  #Toto je komentár lavy ALT+35
+
   Scenario: Pokusny test 1
     Given Toto je nastaveni pocatecnich podminek
+    And Toto je dalsi nastaveni pocatecnich podminek
     When Toto je akcni krok
+    And Toto je dalsi akcni krok
+    And Toto je jeste dalsi akcni krok
     Then Toto je overovaci krok
-    
+    And Toto je dalsi overovaci krok
+
+
+
+
