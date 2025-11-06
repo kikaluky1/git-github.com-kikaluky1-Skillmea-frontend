866052d20398c374391dc4542ef89d8a4c4a0cfb
kikaluky1 <kikaluky1@gmail.com>
2025-11-06 19:55:54 +0100

    pridani testu a code behind

---
+++ src/main/java/Kalkulacka.java
@@ -0,0 +1,24 @@
+import io.cucumber.java.PendingException;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Kalkulacka {
+
+
+    @Given("Uzivatel ma zadany dve cisla {int} a {int}")
+    public void uzivatelMaZadanyDveCislaA(int prvniCislo, int druheCislo) {
+
+    }
+
+
+    @When("Uzivatel secte tato dve cisla")
+    public void uzivatelSecteTatoDveCisla() {
+
+    }
+
+    @Then("Uzivatel vidi na kalkulacce vysledek {int}")
+    public void uzivatelVidiNaKalkulacceVysledek(int predpokladanyVysledek) {
+
+    }
+}
